5edd3c8d8fc99dd19ad3c78d59bf5d3f2d7a68ca
Johnny Karasinski <karasinski@gmail.com>
2013-09-26 13:02:42 -0700

    added solution code for Homework 1 Problem 2

---
+++ Homework/Homework 1 Problem 2/main.f90
@@ -0,0 +1,27 @@
+program minimum_tension
+
+  ! Calculate the position to attach a cable to
+  ! produce minimum tension.
+
+  implicit none
+  real, parameter :: g = 9.81
+  doubleprecision :: min_T, T, W = 200 * g, lc = 8, lp = 8, opt_d, d = 1.0
+
+
+  do d = 1, 7, 0.1
+    ! Calculate the tension on the cable.
+    T = W * lc * lp / (d * sqrt(lp ** 2 - d ** 2))
+
+    ! If this tension is less than the current minimum
+    ! tension, note the tension and distance.
+    if (T < min_T) then
+      min_T = T
+      opt_d = d
+    end if
+
+  end do
+
+  write (*,'(1x,a12,f6.2,a5,5x,a12,f8.2,a8)') &
+      'Optimal d = ',opt_d,' feet','Minimum T = ',min_T,' Newtons'
+
+end program minimum_tension
