b96207706a07a260e80efbdb76acd9c3e678656a
Johnny Karasinski <karasinski@gmail.com>
2013-10-06 17:24:39 -0700

    some additional work on project 1; added lectures

--- Project/Project 1/main.f90
+++ Project/Project 1/main.f90
@@ -1,25 +1,27 @@
 module constants
-  real, parameter, public :: k = 18.8, rho = 8000., c_p = 500.
-  real, parameter, public :: pi = 3.141592654, rot = 30.*pi/180.
+  real*8, parameter, public :: k = 18.8, rho = 8000., c_p = 500.
+  real*8, parameter, public :: pi = 3.141592654, rot = 30.*pi/180.
   integer :: IMAX, JMAX !101 or 501 (2 cases)
 contains
-  subroutine grid_size(length)
+  subroutine SetGridSize(length)
     IMAX = length
     JMAX = length
-  end subroutine grid_size
+  end subroutine SetGridSize
 end module
 
 module GridPointModule
   use constants
+  save
 
   public
   type GridPoint
     integer :: i, j
-    real :: x, xp, y, yp, T
+    real*8 :: x, xp, y, yp, T, tempT
   end type GridPoint
 
 contains
   subroutine initialize(p, i, j)
+    save
     type(GridPoint), intent(inout) :: p
     integer :: i, j
 
@@ -32,56 +34,115 @@ contains
     p%x = p%xp*cos(rot)+(1.-p%yp)*sin(rot)
     p%y = p%yp*cos(rot)+(p%xp)*sin(rot)
 
+    p%T = 3.5
+
   end subroutine initialize
 
   subroutine set_temperature(p, iT)
+    save
     type(GridPoint), intent(inout) :: p
-    real, optional :: iT
+    real*8, optional :: iT
 
-    if (present(iT)) then
-      p%T = iT
-    else
-      p%T = 3.5
-    endif
+    p%T = iT
+
   end subroutine set_temperature
 
-  real function kineticenergy(p)
-    type(GridPoint), intent(in) :: p
-    integer i
-    real :: ke = 0.0
+  subroutine update_temperature(p)
+    ! subroutine should use update gridpoint temperature based on neighbors t
+    save
+    type(GridPoint), intent(inout) :: p
 
-    do i=0,2
-      ke = ke + 1
-    end do
-    kineticenergy = p%T * ke
-    return
-  end function kineticenergy
+    p%T = p%T / 2.
+    !    if (present(iT)) then
+    !      p%T = iT
+    !    else
+    !      p%T = 3.5
+    !    endif
+  end subroutine
+
 end module GridPointModule
 
 program heat
   use constants
   use GridPointModule
 
-  integer :: a, b
-  type (GridPoint) :: Point
-  type (GridPoint), allocatable :: Points(:)
+  integer :: a, b, num, step = 1
+  type (GridPoint), pointer :: Point, upperPoint, lowerPoint, leftPoint, rightPoint
+  type (GridPoint), target, allocatable :: Points(:)
+  real*8, pointer :: Temperature(:)
+  real*8 :: temp, residual = 999
 
-  call grid_size(101)
-  allocate(Points(1: IMAX))
+  call SetGridSize(101)
+  allocate(Points(1: IMAX * IMAX))
+  Temperature => Points%T
 
   open (unit = 1, file = "data.dat")
   do a = 1, IMAX
     do b = 1, IMAX
-      Point = Points((a+b-1))
+      Point => Points( (a - 1) * IMAX + b )
 
-      call set_temperature(Point, 50.)
       call initialize(Point, a, b)
-      write (1,*), Point
-      write (1,*)
+      call set_temperature(Point, DFLOAT(INT(RAND(0)*100)))
+
+!      write (1,'(F10.5, 5X, F10.5, 5X, F10.5)'), Point%x, Point%y, Point%T
     end do
   end do
+
+  do while (residual > 1. .and. step <= 25)
+    residual = maxval(Temperature)
+
+    write(*,*), 'step = ', step
+    write (*, *), 'cell ', maxloc(Temperature), 'has max temp = ', maxval(Temperature)
+
+    do a = 1, IMAX
+      do b = 1, IMAX
+        Point      => Points( (a - 1) * IMAX + (b) )
+
+        temp = 0.
+        num = 0
+
+!       Currently just summing up nearby nodes and averaging.
+!       I probably have the wrong labels here, but the idea should be correct
+!       and as long as I use consistent logic it shouldn't matter?
+
+        if ( ((a - 1) * IMAX + (b - 1)) > 0 .and. ((a - 1) * IMAX + (b - 1)) < IMAX ** 2 ) then
+        upperPoint => Points( (a - 1) * IMAX + (b - 1) )
+        temp = temp + upperPoint%T
+        num = num + 1
+        end if
+
+        if (( (a) * IMAX + (b) ) > 0 .and. ( (a) * IMAX + (b) ) < IMAX ** 2 ) then
+        rightPoint => Points( (a) * IMAX + (b) )
+        temp = temp + rightPoint%T
+        num = num + 1
+        end if
+
+        if (( (a - 1) * IMAX + (b + 1) ) > 0 .and. ( (a - 1) * IMAX + (b + 1) < IMAX ** 2 )) then
+        lowerPoint => Points( (a - 1) * IMAX + (b + 1) )
+        temp = temp + lowerPoint%T
+        num = num + 1
+        end if
+
+        if (( (a - 2) * IMAX + (b) ) > 0 .and. ( (a - 2) * IMAX + (b) < IMAX ** 2 )) then
+        leftPoint  => Points( (a - 2) * IMAX + (b) )
+        temp = temp + leftPoint%T
+        num = num + 1
+        end if
+
+        temp = temp + Point%T
+        num = num + 1
+        temp = temp/dfloat(num)
+        call set_temperature(Point, temp)
+
+!        Currently just divides by two.
+!        call update_temperature(Point)
+
+        write (1,'(I5, 5X, F10.8, 5X, F10.8, 5X, F10.5)'), step, Point%x, Point%y, Point%T
+      end do
+    end do
+
+    step = step + 1
+  end do
+
   close(1)
-
-
-
 end program heat
